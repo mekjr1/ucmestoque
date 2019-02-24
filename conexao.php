8c76ca0036f4254cd7a0d5df42730ca706f1941d
mekjr1 <gafar_meque@hotmail.com>
2019-02-24 20:06:16 +0200

    adding to github to deploy

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
 
-define('Host', 'localhost');
-define('User', 'root');
-define('Password', '');
+define('Host', 'den1.mysql6.gear.host');
+define('User', 'ucmestoque');
+define('Password', 'Wu2D7?~zl12l');
 define('DBName', 'ucmestoque');
 
  $conn= new PDO('mysql:host=' . Host . ';dbname=' . DBName . ';', User, Password);
